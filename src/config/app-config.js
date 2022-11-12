d6017e0736dcb96c8e646c635776927a9c39acc7
ekslffh <ekslffh@gmail.com>
2022-11-13 01:46:46 +0900

    백엔드 배포 후 설정변경

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -3,7 +3,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-    backendHost = "http://localhost:8080";
+    backendHost = "http://prod-hsap-backend.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
