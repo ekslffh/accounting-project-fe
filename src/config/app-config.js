be6b040f82d68135cb7bc7bac12809556682c49c
ekslffh <ekslffh@gmail.com>
2022-11-13 02:36:31 +0900

    요청url 수정

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -2,7 +2,10 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
+// http://localhost:8080
 if (hostname === "localhost") {
+    backendHost = "http://prod-hsap-backend.ap-northeast-2.elasticbeanstalk.com";
+} else {
     backendHost = "http://prod-hsap-backend.ap-northeast-2.elasticbeanstalk.com";
 }
 
