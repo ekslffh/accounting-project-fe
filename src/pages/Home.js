5c5da4b5e7942d438ba9855d0f58cd8e28be2173
ekslffh <ekslffh@gmail.com>
2022-11-16 22:58:23 +0900

    Home.js 개인페이지 이동 시 undefined->현재년도변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Home(props) {
-    const [year, ] = React.useState(props.match.params.year);
+    const [year, setYear] = React.useState(props.match.params.year);
     if (year !== undefined && isNaN(year)) return window.location.href = "/"
     const role = localStorage.getItem("role");
     const department = localStorage.getItem("department");
+    if (year === undefined) setYear(new Date().getFullYear()); 
     if (role === "USER") window.location.href = "/user/" + year;
     else if (role === "LEADER") window.location.href = "/leader/" + department + "/" + year;
     else if (role === "ADMIN") window.location.href = "/admin";
