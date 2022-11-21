c90d9c7ffe2838998df1300db4c6b5983a0351b3
ekslffh <ekslffh@gmail.com>
2022-11-22 00:59:19 +0900

    월별지출은 전체 히스토리 보내주기

--- src/pages/Leader.js
+++ src/pages/Leader.js
@@ -281,7 +281,7 @@ export default function Leader(props) {
                     height: 240,
                   }}
                 >
-                  <MonthlyChart histories={filteredHistories} year={year} />
+                  <MonthlyChart histories={histories} year={year} />
                 </Paper>
               </Grid>
               <Grid item xs={12} md={4} lg={3}>
